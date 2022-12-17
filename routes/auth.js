64180cc5caff491cd0010955e26942746cfc6529
Rahul Kumar <rahul.kumar0850@gmail.com>
2022-12-18 00:48:32 +0530

    Request body validator

--- routes/auth.js
+++ routes/auth.js
@@ -1,19 +1,40 @@
 const express = require("express");
 const UserSchema = require("../models/User");
 const router = express.Router();
+const { body, validationResult } = require("express-validator");
 
 // Create user
-router.post("/", async (req, res) => {
-  try {
-    const user = UserSchema(req.body);
-    const dbStatus = await user.save();
-    console.log("Save status : "+dbStatus);
-    res.status(200);
-    res.send(req.body);
-  } catch (err) {
-    console.log(err);
-    res.status(500).json(err);
+router.post(
+  "/",
+  [
+    body("name","Name should contain at least 3 characters.").isLength({ min: 3 }),
+    body("email","Enter a valid email.").isEmail(),
+    body("mobile","Enter a valid mobile").escape()
+    .exists({checkFalsy: true})
+    .isLength({min: 10, max:10})
+    .matches(/^([0|\+[0-9]{1,5})?([7-9][0-9]{9})$/),
+    body("password","Password should contain minimum eight characters, at least one letter, one number and one special character.").escape()
+    .exists({checkFalsy: true})
+    .isLength({min: 8})
+    .matches(/^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)(?=.*[@$!%*?&])[A-Za-z\d@$!%*?&]{8,}$/)
+  ],
+  async (req, res) => {
+    try {
+      let errors = validationResult(req);
+      console.log("Validation Result : "+errors.isEmpty());
+      if (!errors.isEmpty()) {
+        return res.status(400).json({ errors: errors.array() });
+      }
+      const user = UserSchema(req.body);
+      const dbStatus = await user.save();
+      console.log("Save status : " + dbStatus);
+      res.status(200);
+      res.send(req.body);
+    } catch (err) {
+      console.log(err);
+      res.status(500).json(err);
+    }
   }
-});
+);
 
 module.exports = router;
