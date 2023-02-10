3f43bd06c2d6c5505d48eebb3f04658f4d7e203a
Rahul Kumar <rahul.kumar0850@gmail.com>
2023-02-10 21:39:14 +0530

    01 class - Intro

---
+++ 01/intro.ts
@@ -0,0 +1,6 @@
+
+let user={name:"Rahul Kumar",age:31}
+
+console.log(user.name)
+console.log(user.age)
+console.log(user.email," This shows error in typescript because email attribute is not present in the user object , but still .js file can be generated.") 
