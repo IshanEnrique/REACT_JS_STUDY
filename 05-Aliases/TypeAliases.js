e38d37ac62a6a12205881d268da81c8e8c0bfabe
Rahul Kumar <rahul.kumar0850@gmail.com>
2023-02-12 22:10:47 +0530

    Type alias , readonly and optional attribute

--- 05-Aliases/TypeAliases.js
+++ 05-Aliases/TypeAliases.js
@@ -1,4 +1,10 @@
+"use strict";
+exports.__esModule = true;
 function createUser(user) {
     return user;
 }
-createUser({ name: "Rahul", email: "rahul@bharatkadam.com", age: 31, isLoggedIn: false });
+// Declaring variable of type User -
+var user = { _id: "0001", name: "Rahul", email: "rahul@bharatkadam.com", age: 31, isLoggedIn: false };
+//  Let try to modify the read only value _id . It does'nt allow us modify read only values
+// user._id="1233333"
+createUser(user);
--- 05-Aliases/TypeAliases.ts
+++ 05-Aliases/TypeAliases.ts
@@ -1,9 +1,11 @@
 
 type User={
+    readonly _id:string; // Once initialized , this read only can not be re-assigned and its for read only purposes
     name:string;
     email:string;
     age:number;
     isLoggedIn:boolean;
+    cardDetails ?: string // ?: is used to define optional attribute , if you provide then good or else if don't provide then also not any problem
 }
 
 function createUser(user:User):User{
@@ -11,6 +13,14 @@ function createUser(user:User):User{
     return user;
 }
 
-createUser({name:"Rahul",email:"rahul@bharatkadam.com",age:31,isLoggedIn:false})
+// Declaring variable of type User -
+
+let user: User = {_id: "0001", name:"Rahul",email:"rahul@bharatkadam.com",age:31,isLoggedIn:false}
+
+//  Let try to modify the read only value _id . It does'nt allow us modify read only values
+
+// user._id="1233333"
+
+createUser(user)
 
 export {}
